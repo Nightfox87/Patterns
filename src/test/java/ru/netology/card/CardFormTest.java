2095b33f65368f4a37a510708a934bd1f2d5cd8e
Дина Абрамова <nightfox87@mail.ru>
2022-08-21 19:10:19 +0300

    Test and data added

---
+++ src/test/java/ru/netology/card/CardFormTest.java
@@ -0,0 +1,58 @@
+package ru.netology.card;
+
+import com.codeborne.selenide.Condition;
+import com.codeborne.selenide.Configuration;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.Keys;
+import ru.netology.card.DataGenerator;
+
+import java.time.Duration;
+
+import static com.codeborne.selenide.Condition.visible;
+import static com.codeborne.selenide.Selectors.byText;
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
+import static ru.netology.card.DataGenerator.generateDate;
+
+public class CardFormTest {
+
+    @BeforeEach
+    void setup() {
+        Configuration.holdBrowserOpen = true;
+        open("http://localhost:9999");
+    }
+
+    @Test
+    @DisplayName("Should successful plan and replan meeting")
+    void shouldSuccessfulPlanAndReplanMeeting() {
+        var validUser = DataGenerator.Registration.generateUser("ru");
+        var daysToAddForFirstMeeting = 4;
+        var firstMeetingDate = generateDate(daysToAddForFirstMeeting);
+        var daysToAddForSecondMeeting = 7;
+        var secondMeetingDate = generateDate(daysToAddForSecondMeeting);
+        $("[data-test-id='city'] input").setValue(validUser.getCity());
+        $("[placeholder='Дата встречи']").sendKeys(Keys.COMMAND + "a");
+        $("[placeholder='Дата встречи']").sendKeys(Keys.BACK_SPACE);
+        String planningDate = generateDate(daysToAddForFirstMeeting);
+        $("[placeholder='Дата встречи']").setValue(planningDate);
+        $("[data-test-id='name'] input").setValue(validUser.getName());
+        $("[data-test-id='phone'] input").setValue(validUser.getPhone());
+        $("[data-test-id='agreement'] span").click();
+        $(byText("Запланировать")).click();
+        $(byText("Успешно!")).shouldBe(visible, Duration.ofSeconds(15));
+        $(".notification__content")
+                .shouldHave(Condition.text("Встреча успешно запланирована на " + planningDate), Duration.ofSeconds(15))
+                .shouldBe(Condition.visible);
+        $("[placeholder='Дата встречи']").sendKeys(Keys.COMMAND + "a");
+        $("[placeholder='Дата встречи']").sendKeys(Keys.BACK_SPACE);
+        String planningDate2 = generateDate(daysToAddForSecondMeeting);
+        $("[placeholder='Дата встречи']").setValue(planningDate2);
+        $(byText("Запланировать")).click();
+        $(byText("Перепланировать")).click();
+        $(".notification__content")
+                .shouldHave(Condition.text("Встреча успешно запланирована на " + planningDate2), Duration.ofSeconds(15))
+                .shouldBe(Condition.visible);
+    }
+}
